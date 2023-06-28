20d4349d3fc370f2b84b9539bfbecf744f6cd7c0
Edwin Mercedes <edwinavilez87@gmail.com>
2023-06-28 09:26:25 -0400

    Actualizacion

--- src/purchase/services/purchase-product.service.ts
+++ src/purchase/services/purchase-product.service.ts
@@ -2,8 +2,11 @@ import { DeleteResult, UpdateResult } from "typeorm";
 import { BaseService } from "../../config/base.service";
 import { PurchaseProductDTO } from "../dto/purchase-product.dto";
 import { PurchaseProductEntity } from "../entities/purchases-products.entity";
+import { ProductService } from "../../product/services/product.service";
 export class PurchaseProductService extends BaseService<PurchaseProductEntity> {
-  constructor() {
+  constructor(
+    private readonly productService: ProductService = new ProductService()
+  ) {
     super(PurchaseProductEntity);
   }
 
@@ -13,12 +16,15 @@ export class PurchaseProductService extends BaseService<PurchaseProductEntity> {
   async findPurchaseProductById(
     id: string
   ): Promise<PurchaseProductEntity | null> {
-    return (await this.execRepository).findOneBy({ id })
+    return (await this.execRepository).findOneBy({ id });
   }
   async createPurchaseProduct(
     body: PurchaseProductDTO
   ): Promise<PurchaseProductEntity> {
-    return (await this.execRepository).save(body);
+    const newPP = (await this.execRepository).create(body);
+    const prod = await this.productService.findCustomerById(newPP.product.id);
+    newPP.totalPrice = prod!.price * newPP.quantityProduct;
+    return (await this.execRepository).save(newPP);
   }
   async deletePurchaseProduct(id: string): Promise<DeleteResult> {
     return (await this.execRepository).delete({ id });
